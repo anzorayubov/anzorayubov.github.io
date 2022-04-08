ab2191d47c2bd766f7d2873313a61269a4b82281
Anzor Aiubov <Anzor_Aiubov@epam.com>
2022-04-08 16:06:31 +0300

    added loader and popup after submitting form

--- js/mail.js
+++ js/mail.js
@@ -1,14 +1,15 @@
 $(document).ready(function () {
     $("form").submit(function () {
         var th = $(this);
+        $('#loading').show();
         $.ajax({
             type: "POST",
             url: "mail.php",
             data: th.serialize()
         }).done(function () {
-            setTimeout(function () {
-                th.trigger("reset");
-            }, 1000);
+            $('#loading').hide();
+            $('#success-window').show();
+            setTimeout(() => { th.trigger("reset") }, 1000);
         });
         return false;
     });
