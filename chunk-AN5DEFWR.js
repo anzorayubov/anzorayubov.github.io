e038472a231bc0ad28d5600d6f6bc099c983ae3b
Anzor <48889137+anzorayubov@users.noreply.github.com>
2025-01-28 14:44:08 +0300

    admin page: show products

---
+++ chunk-AN5DEFWR.js
@@ -0,0 +1,1 @@
+import{Q as r,_ as o,da as s,g as n,la as u}from"./chunk-FOUY5J7Y.js";function i(t){t||(s(i),t=o(u));let c=new n(e=>t.onDestroy(e.next.bind(e)));return e=>e.pipe(r(c))}export{i as a};
